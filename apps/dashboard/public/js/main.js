5fc4258cf1acdf8c9c0a930bbe1d3f39a46abb76
andtan <jornandretangen@gmail.com>
2012-12-28 02:56:49 +0100

    frontend dashboard js work

--- apps/dashboard/public/js/main.js
+++ apps/dashboard/public/js/main.js
@@ -1,12 +1,18 @@
-var SideBarView = require( './views/SideBar' )
+var SideBarView           = require( './views/SideBar' ),
+    LogSessionCollection  = require( './collections/LogSession' )
 
 
+window.app = {}
 
+app.logSessionCollection = new LogSessionCollection()
 
 $(document).ready(function() {
 
-  sideBarView = new SideBarView({ el: '#sidebar'})
+  app.sideBarView = new SideBarView({ el: '#sidebar'})
 
-
+  setTimeout(function() {
+    app.logSessionCollection.startSync()
+  }, 100)
+  
 })
 
