f31be2650703d0ddb4015893baefeb08c86d6d30
andtan <jornandretangen@gmail.com>
2012-12-29 03:33:00 +0100

    preittify object output. need to get undefined values to show

--- apps/dashboard/public/js/views/LogMessage.js
+++ apps/dashboard/public/js/views/LogMessage.js
@@ -10,8 +10,55 @@ var LogMessageView = Backbone.View.extend({
   template: _.template( $("#logmessage-view-template").html() ),
 
   render: function() {
+    var messages, objectHTML, i
+
     this.$el.addClass( this.model.get('type') )
-    this.$el.html( this.template(this.model.toJSON()) )
+
+    messages = this.model.get( 'messages' )
+    objectHTML = ""
+
+    messages.forEach(function( message, index ) {
+      objectHTML += this.createObjectHTML( message.value )
+      if( index + 1 < messages.length )
+        objectHTML += ", "
+    }, this)
+    
+    this.$el.html( objectHTML )
+  },
+
+  createObjectHTML: function( object ) {
+    var html, type, key, val, i
+
+    type = typeof object
+    html = "<span class='value " + type + "'>"
+
+    if( type === "object" ) {
+
+      i = 0
+      
+      html += "{ "
+      for( key in object ) {
+        i++
+        val = object[ key ]
+        html += "<span class='key'>" +key + "</span> : "
+        html += this.createObjectHTML( val )
+        if( i < Object.keys(object).length )
+          html += ", "
+      }
+      html += " }"
+
+    } else if(type == "string" ) {
+      
+      html += '"' + object + '"'
+
+    } else {
+      
+      html += object
+
+    }
+
+    html += "</span>"
+    return html
   }
 
 })
